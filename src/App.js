0886129d1e3dd69d488fbf34bc7494c33575d9fa
Blake Neely <blakeneely21@gmail.com>
2019-12-09 13:10:46 -0600

    initial file upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+// Import components here as we build them
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        {/* Here we will render a navbar, main-game, and possibly a footer */}
+      </div>
+    )
+  }
+};
+
+export default App;
