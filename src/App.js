bd707bca4cfa3f60f843d62da9d4fc04fcbe6d58
Blake Neely <blakeneely21@gmail.com>
2019-12-10 22:03:35 -0600

    built out logic to run game, fixed various callbacks in components to get to run

--- src/App.js
+++ src/App.js
@@ -6,13 +6,76 @@ import characters from "./characters.json";
 import "./App.css";
 // Import components here as we build them
 
+  // Function to shuffle cards 
+const shuffleCards = arr => (
+  arr
+    .map(a => [Math.random(), a])
+    .sort((a, b) => a[0] - b[0])
+    .map(a => a[1]) 
+);
+
 class App extends Component {
+  state = {
+    characters: shuffleCards(characters),
+    clickedIds: [],
+    score: 0,
+    goal: 15,
+    status: ""
+  };
+
+  runGame = id => {
+    let clickedIds = this.state.clickedIds;
+
+    if(clickedIds.includes(id)) {
+      this.setState({ 
+        clickedIds: [], 
+        score: 0, 
+        status:  "Game Over! You lost. Click to play again!" 
+      });
+      return
+    }
+    else {
+      clickedIds.push(id)
+
+      if(clickedIds.length === 15) {
+        this.setState({
+          score: 8, 
+          status: "You won! Click to play again!",
+          clickedIds: []
+        })
+        return
+      };
+
+      this.setState({ 
+        characters: shuffleCards(characters), 
+        clickedIds, 
+        score: clickedIds.length, 
+        status: " " 
+      });
+    }
+  };
+
+
+
   render() {
     return (
       <div className='App'>
         {/* Here we will render a navbar, main-game, and possibly a footer */}
-        <Navbar />
-        <Game />
+        <Navbar 
+          score={this.state.score}
+          status={this.state.status}
+          won={this.state.gamesWon}
+        />
+        <div className="container">
+        {this.state.characters.map(character => (
+            <Cards
+              runGame={this.runGame}
+              id={character.id}
+              key={character.id}
+              image={character.image}
+            />
+          ))}
+        </div>
         <Footer />
       </div>
     )
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,2 +1,15 @@
 import React from "react";
 import "./style.css";
+
+const Footer = () => {
+    return (
+    <footer>
+        <p>
+            Designed by Blake Neely
+        </p>
+    </footer>
+
+    )
+};
+
+export default Footer;
