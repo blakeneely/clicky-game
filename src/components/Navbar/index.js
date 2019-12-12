3ebbc54add6b4bc445338fd34abdb930cbb9f3eb
Blake Neely <blakeneely21@gmail.com>
2019-12-11 19:19:41 -0600

    troubleshooting image paths

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,7 +5,7 @@ const Navbar = props => {
     return (
     <nav className="navbar sticky-top text-center">
         <div className="order-0">
-            <img src="/images/star_wars_logo.png" height="60" width="90" className="d-inline-block align-top"></img>
+            <img src="./images/star_wars_logo.png" height="60" width="90" className="d-inline-block align-top"></img>
             <h4 className="nav-text">Clicky-Game</h4>
         </div>
         <div className="order-1">
